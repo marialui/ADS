38cb1c0363161269ad02f4a50a01fcf03ec33704
marialui <45207676+marialui@users.noreply.github.com>
2020-05-26 11:23:12 +0200

    Add files via upload

--- fractionknapsack.py
+++ fractionknapsack.py
@@ -9,19 +9,18 @@ def knap(item,v,w,W):
         ratio.append((val,i))
     #print(ratio)
     ratio.sort(reverse=True,key=lambda x: x[0]) #sorts from the higest to the lowest according to ratio
-    print(ratio)
     knap=0 #peso aggiornato del sacco
     knapel=[] #lista di elementi messi nel sacco
-    for el in ratio:
-        if knap + w[el[1]] < W:
-            knap=knap + w[el[1]]
-            knapel.append(item[el[1]])
-        else:
-            diff= W-knap
-            fract= float(diff/w[el[1]])
-            knapel.append('the %s of ' %(round((fract),2)) + item[el[1]])
-            knap= knap + float(fract)* float( w[el[1]])
 
-    print(knap)
-    print(knapel)
+    while knap < W:
+        for el in ratio:
+            if knap + w[el[1]] < W:
+                knap=knap + w[el[1]]
+                knapel.append(item[el[1]])
+            else:
+                diff = W - knap
+                fract= float(diff/w[el[1]])
+                knapel.append('the %s of ' %(round((fract),2)) + item[el[1]])
+                knap= knap + float(fract)* float( w[el[1]])
+    return knapel
 print(knap(gioielli,valore,peso,capienza))
